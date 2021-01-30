49e016b4d67290fccfedbe301c99a4665d19029a
Belastotski <di.belost@gmail.com>
2021-01-30 02:59:38 +0100

    add: index.js function min

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+exports.min = array => {
+     !arguments.length?  0: Math.min(...array)
+}
+
+exports.max = function max (array) {
+  return 0;
+}
+
+exports.avg = function avg (array) {
+  return 0;
+}
